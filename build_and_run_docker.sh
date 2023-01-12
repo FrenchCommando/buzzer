77020d549b9989194bcc1d76fcc8124eb3ca47eb
FrenchCommando <martialren@gmail.com>
2023-01-12 02:39:29 +0000

    docker mode change

--- build_and_run_docker.sh
+++ build_and_run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build -t buzzerbuild .
-docker run -d --rm -p 1337:1337 --cap-add=SYS_ADMIN --cap-add=NET_ADMIN --net=host -it buzzerbuild
+docker run -d -p 1337:1337 --cap-add=SYS_ADMIN --cap-add=NET_ADMIN --net=host --restart unless-stopped buzzerbuild
